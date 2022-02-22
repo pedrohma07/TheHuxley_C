849553cc241e196b7a5520010737e717b9ff42c3
Pedro Henrique <87721963+pedrohma07@users.noreply.github.com>
2022-02-22 08:54:24 -0300

    Add files via upload

---
+++ 1000 Números do Tio Willy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+int main(){
+    int v[1000], n;
+    int count = 0, countI = 0;
+    do{   
+        for (int i = 0; i < 1000; i++) {
+            scanf("%d", &v[i]);
+            countI++;
+            if(v[i] == -1)
+                break;
+        }
+        if(v[countI-1] == -1)
+            break;
+        scanf("%d", &n);
+        for (int i = 0; i < countI; i++) {
+            if(v[i] == n)
+                count++;
+        }
+        printf("%d appeared %d times\n", n, count);
+        if(v[countI-1] == -1)
+            break;
+        count = 0;
+        countI = 0;
+    }while(v[countI -1 ] != -1);
+
+
+    return 0;
+}
